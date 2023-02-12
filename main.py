0e3198819adf0cc9f01a5bb59ce066d2d99b521f
Saltal27 <omarmobarak53@gmail.com>
2023-02-12 22:41:10 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from tkinter import *
+from random import choice
+
+# ---------------------------- FLIPPING CARDS ------------------------------- #
+lang = 1
+
+
+def flip():
+    global card
+    global lang
+
+    if lang == 2:
+        # switch to Deutsche card
+        card.config(file="Rounded Rectangle 2.png")
+        lang = 1
+    else:
+        # switch to English card
+        card.config(file="Rounded Rectangle 1.png")
+        lang = 2
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -7,22 +25,25 @@ window.title("Flashy")
 window.minsize(width=400, height=400)
 window.config(padx=50, pady=50, background="#3944Bc")
 
-english_card = PhotoImage(file="Rounded Rectangle 1.png")
-english_card_canvas = Canvas(height=400, width=550, highlightthickness=0, background="#3944Bc")
-english_card_canvas.create_image(275, 200, image=english_card)
-english_card_canvas.grid(row=0, column=0, columnspan=2)
+card = PhotoImage(file="Rounded Rectangle 2.png")
+card_canvas = Canvas(width=550, height=400, highlightthickness=0, background="#3944Bc")
+card_canvas.create_image(275, 200, image=card)
+card_canvas.grid(row=0, column=0, columnspan=3)
 
-deutsche_card = PhotoImage(file="Rounded Rectangle 2.png")
-deutsche_card_canvas = Canvas(height=400, width=550, highlightthickness=0, background="#3944Bc")
-deutsche_card_canvas.create_image(275, 200, image=deutsche_card)
-deutsche_card_canvas.grid(row=0, column=0, columnspan=2)
-
+# Buttons
 tick_mark = PhotoImage(file="tick mark.png")
-tick_mark_button = Button(image=tick_mark, background="#3944Bc", borderwidth=0, activebackground="#3944Bc")
+tick_mark_button = Button(image=tick_mark, background="#3944Bc", borderwidth=0,
+                          activebackground="#3944Bc")
 tick_mark_button.grid(row=1, column=0)
 
+flip_pic = PhotoImage(file="flip pic.png")
+flip_button = Button(image=flip_pic, background="#3944Bc", borderwidth=0,
+                     activebackground="#3944Bc", command=flip)
+flip_button.grid(row=1, column=1)
+
 cross_mark = PhotoImage(file="cross mark.png")
-cross_mark_button = Button(image=cross_mark, background="#3944Bc", borderwidth=0, activebackground="#3944Bc")
-cross_mark_button.grid(row=1, column=1)
+cross_mark_button = Button(image=cross_mark, background="#3944Bc", borderwidth=0,
+                           activebackground="#3944Bc")
+cross_mark_button.grid(row=1, column=2)
 
 window.mainloop()
