d3b7a1632c6cba61649dd3fd306312a9948d2a08
Saltal27 <omarmobarak53@gmail.com>
2023-02-13 18:19:44 +0300

    final

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
+import random
 from tkinter import *
-from random import choice
+import pandas
+import json
+
+
+# ---------------------------- RESET DICTIONARY ------------------------------- #
+def reset_dict():
+    raw_data = pandas.read_csv("1000 most common words.csv")
+    with open("data.json", "w") as raw_data_file:
+        json.dump(raw_data.to_dict(), raw_data_file, indent=4)
+
+
+# ---------------------------- PICKING WORDS ------------------------------- #
+word_index = 0
+
+
+def random_pick():
+    global word_index
+    global lang
+    picking = True
+
+    while picking:
+        word_index = random.randint(0, 999)
+        try:
+            with open("data.json") as data_file:
+                data = json.load(data_file)
+                if lang == 1:
+                    card_word.config(text=data["Deutsche"][f"{word_index}"])
+                else:
+                    card_word.config(text=data["English"][f"{word_index}"])
+        except KeyError:
+            pass
+        else:
+            picking = False
+
+
+# ---------------------------- TICK ------------------------------- #
+def tick():
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        del data["Deutsche"][f"{word_index}"]
+        del data["English"][f"{word_index}"]
+
+    with open("data.json", "w") as data_file:
+        json.dump(data, data_file, indent=4)
+
+    random_pick()
+
 
 # ---------------------------- FLIPPING CARDS ------------------------------- #
 lang = 1
@@ -12,10 +59,16 @@ def flip():
     if lang == 2:
         # switch to Deutsche card
         card.config(file="Rounded Rectangle 2.png")
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+            card_word.config(text=data["Deutsche"][f"{word_index}"])
         lang = 1
     else:
         # switch to English card
         card.config(file="Rounded Rectangle 1.png")
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+            card_word.config(text=data["English"][f"{word_index}"])
         lang = 2
 
 
@@ -33,7 +86,7 @@ card_canvas.grid(row=0, column=0, columnspan=3)
 # Buttons
 tick_mark = PhotoImage(file="tick mark.png")
 tick_mark_button = Button(image=tick_mark, background="#3944Bc", borderwidth=0,
-                          activebackground="#3944Bc")
+                          activebackground="#3944Bc", command=tick)
 tick_mark_button.grid(row=1, column=0)
 
 flip_pic = PhotoImage(file="flip pic.png")
@@ -43,7 +96,17 @@ flip_button.grid(row=1, column=1)
 
 cross_mark = PhotoImage(file="cross mark.png")
 cross_mark_button = Button(image=cross_mark, background="#3944Bc", borderwidth=0,
-                           activebackground="#3944Bc")
+                           activebackground="#3944Bc", command=random_pick)
 cross_mark_button.grid(row=1, column=2)
+
+reset_pic = PhotoImage(file="reset pic.png")
+reset = Button(image=reset_pic, background="#3944Bc", borderwidth=0,
+               activebackground="#3944Bc", command=reset_dict)
+reset.grid(row=2, column=1)
+
+# Labels
+card_word = Label(text="", font=("Arial Rounded MT", 30, "bold"), foreground="#3944Bc", background="white")
+card_word.grid(row=0, column=0, columnspan=3)
+random_pick()
 
 window.mainloop()
